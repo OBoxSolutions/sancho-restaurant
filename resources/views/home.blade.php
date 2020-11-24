6b666b93196d14db889b056c750e01553e0ca495
Jose Garcia <jogarcia@tuta.io>
2020-11-24 09:25:14 -0500

    Make basic views files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,27 @@
 @section('route', Route::currentRouteName())
 
 @section('content')
-    <vue-back-to-top></vue-back-to-top>
+
+<vue-back-to-top></vue-back-to-top>
+<bootstrap-carousel></bootstrap-carousel>
+
+<section>
+    <vue-section></vue-section>
+</section>
+<section>
+    <vue-section></vue-section>
+</section>
+<section>
+    <vue-section></vue-section>
+</section>
+<section>
+    <vue-section></vue-section>
+</section>
+<section>
+    <vue-section></vue-section>
+</section>
+<section>
+    <vue-section></vue-section>
+</section>
+
 @endsection
---
+++ resources/views/reserve.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('route', Route::currentRouteName())
+
+@section('content')
+<vue-back-to-top></vue-back-to-top>
+<section>
+    <vue-section></vue-section>
+</section>
+@endsection
