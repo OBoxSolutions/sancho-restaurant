ef85c371a7985810e5cf3f0899c9debaeadae4df
Jose Garcia <jogarcia@tuta.io>
2020-11-24 20:37:26 -0500

    Simplify footer

--- resources/js/components/AppFooter.vue
+++ resources/js/components/AppFooter.vue
@@ -5,69 +5,34 @@
         <div class="row">
           <div class="col-12 col-md-4 text-center">
             <img
-              src="/images/kampeki-kun-name.png"
+              src="/assets/logo.webp"
               alt="Logo de ZaruArquitectura"
               class="site-logo text-center"
-              style="width:80px;"
+              style="width: 240px"
             />
-            <p>
-              Natus eget occaecati, lobortis, vestibulum nam eros, risus lacinia
-              lacus.Lorem accusantium.
-            </p>
           </div>
           <div class="col-12 col-md-4">
-            <h3 class="padding-bottom-40 text-center">Contact details</h3>
+            <h3 class="padding-bottom-40 text-center">Detalles de contactos</h3>
             <ul class="fa-ul margin-left-25">
               <li class="padding-bottom-20 padding-left-5">
                 <span class="fa-li">
-                  <i class="fa fa-map-marker"></i>
+                  <i class="mdi mdi-phone"></i>
                 </span>
-                125, Central Square,New York
+                05 3087308
               </li>
               <li class="padding-bottom-20 padding-left-5">
-                <span class="fa-li">
-                  <i class="fas fa-phone"></i>
-                </span>
-                91 123-456-7890/91
-              </li>
-              <li class="padding-bottom-20 padding-left-5">
-                <span class="fa-li">
-                  <i class="fa fa-mobile"></i>
-                </span>
-                (002) 123-4567
-              </li>
-              <li class="padding-bottom-20 padding-left-5">
-                <span class="fa-li">
-                  <i class="fa fa-envelope"></i>
-                </span>
-                Info@Example.Com
+                <span class="mdi mdi-email"></span>
+                lorem@gmail.com
               </li>
             </ul>
           </div>
-          <div class="col-12 col-md-4">
-            <h3 class="padding-bottom-40 text-center">Important links</h3>
-            <ul class="fa-ul margin-left-25">
-              <li class="padding-bottom-20 padding-left-5">
-                <a href = "/about" class="color-principal">
-                  <span class="fa-li">
-                    <i class="fa fa-link"></i>
-                  </span>
-                  About us
-                </a>
-              </li>
-              <li class="padding-bottom-20 padding-left-5">
-                <a href class="color-principal">
-
-                  <span class="fa-li">
-                    <i class="fa fa-link"></i>
-                  </span>
-                  Go to the contact page
-                </a>
-              </li>
-            </ul>
+          <div class="col-12 col-md-4 mt-0 pt-0">
+            <social-links></social-links>
           </div>
         </div>
-        <div class="text-center padding-top-30 padding-bottom-30">Copyright © 2020 Se no Kanpeki-Sa</div>
+        <div class="text-center padding-top-30 padding-bottom-30">
+          Copyright © 2020 Sancho Restaurant
+        </div>
       </div>
     </div>
   </footer>
