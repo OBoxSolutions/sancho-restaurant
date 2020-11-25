05c2e09d0e9ca395ede1ffba35f7a2fd1f690e73
Jose Garcia <jogarcia@tuta.io>
2020-11-24 20:37:16 -0500

    Remove social icons from app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,9 +33,6 @@
         <main>
             @yield('content')
         </main>
-        <vue-section :has-padding="false" class="bg-dark">
-            <social-links></social-links>
-        </vue-section>
         <app-footer></app-footer>
     </div>
 </body>
