773ab8a99967e1f467d1bf7713a0bcc91fe24070
Jose Garcia <jogarcia@tuta.io>
2020-11-30 17:27:02 -0500

    Set boilerplate content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,22 +8,65 @@
 <bootstrap-carousel></bootstrap-carousel>
 
 <section>
-    <vue-section></vue-section>
+    <vue-section>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="{{ asset('assets/phone.svg') }}" class="img-fluid" alt="A phone image" srcset="">
+            </div>
+            <div class="col-md-6">
+                <h1>Llame ahora y reserve</h1>
+                <h1>+69 6948694</h1>
+            </div>
+        </div>
+    </vue-section>
 </section>
 <section>
-    <vue-section></vue-section>
+    <vue-section>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="{{ asset('assets/phone.svg') }}" class="img-fluid" alt="Imagen de noticias" srcset="">
+            </div>
+            <div class="col-md-6">
+                <h1>Llame ahora y reserve</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum molestias ullam fuga quas soluta deleniti voluptatum odio, voluptas impedit sit id eveniet veritatis sunt delectus ipsa, facilis nihil, officia esse.</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum molestias ullam fuga quas soluta deleniti voluptatum odio, voluptas impedit sit id eveniet veritatis sunt delectus ipsa, facilis nihil, officia esse.</p>
+                <a href="#">
+                    <button class="btn btn-danger">Ir a noticias</button>
+                </a>
+            </div>
+        </div>
+    </vue-section>
 </section>
 <section>
-    <vue-section></vue-section>
+    <vue-section>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="{{ asset('assets/phone.svg') }}" class="img-fluid" alt="Imagen de noticias" srcset="">
+            </div>
+            <div class="col-md-6">
+                <h1>Regístrese para recibir ofertas y especiales a su correo</h1>
+                <a href="#">
+                    <button class="btn btn-danger">Ir a noticias</button>
+                </a>
+            </div>
+        </div>
+    </vue-section>
 </section>
 <section>
-    <vue-section></vue-section>
+    <vue-section>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="{{ asset('assets/phone.svg') }}" class="img-fluid" alt="Imagen de noticias" srcset="">
+            </div>
+            <div class="col-md-6">
+                <h1>Llame ahora y reserve</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum molestias ullam fuga quas soluta deleniti voluptatum odio, voluptas impedit sit id eveniet veritatis sunt delectus ipsa, facilis nihil, officia esse.</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum molestias ullam fuga quas soluta deleniti voluptatum odio, voluptas impedit sit id eveniet veritatis sunt delectus ipsa, facilis nihil, officia esse.</p>
+                <a href="#">
+                    <button class="btn btn-danger">Ir a noticias</button>
+                </a>
+            </div>
+        </div>
+    </vue-section>
 </section>
-<section>
-    <vue-section></vue-section>
-</section>
-<section>
-    <vue-section></vue-section>
-</section>
-
 @endsection
