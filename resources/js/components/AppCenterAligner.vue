039bb4baee9926e43ef8050567ef5ebfeccaa163
Jose Garcia <jogarcia@tuta.io>
2020-12-02 18:07:24 -0500

    Create center aligner component

---
+++ resources/js/components/AppCenterAligner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+}
+</style>
