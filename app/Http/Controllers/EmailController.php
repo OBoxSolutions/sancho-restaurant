42d51ab55ad89a6165f6b936a5d06b54a98ee9ce
Jose Manuel Cassola <jcassola@uclv.cu>
2020-12-09 23:35:22 -0500

    Make store email method

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Email;
 use Illuminate\Http\Request;
 
 class EmailController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|unique:users,email',
+        ]);
+
+        Email::create($request->all());
+
+        return redirect()->route('home');
+    }
 }
