adf321dbd9a208cd3233dc1b49989c19b28300ef
Jose Garcia <jogarcia@tuta.io>
2020-12-13 20:44:56 -0500

    Add sendEmail form

---
+++ resources/views/sendEmail.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('route', Route::currentRouteName())
+
+@section('content')
+<vue-back-to-top></vue-back-to-top>
+<section>
+    <vue-section>
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="emailBody">Cuerpo del correo</label>
+                <textarea class="form-control" name="emailBody" id="emailBody" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+    </vue-section>
+</section>
+@endsection
