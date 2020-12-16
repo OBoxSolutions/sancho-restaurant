49f8f5703ed78f731cb56171b896b9c92a584c08
Jose Garcia <jogarcia@tuta.io>
2020-12-15 20:22:35 -0500

    Rename returning view

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::view('contact', 'contact')->name('contact');
 Route::view('menu', 'menu')->name('menu');
 Route::view('reserve', 'reserve')->name('reserve');
 
-Route::view('email/create', 'email')->name('email');
+Route::view('email/create', 'subscribe')->name('email');
 Route::post('email/store', 'EmailController@store');
 Route::view('email/send', 'sendEmailForm')->name('emailSend');
 Route::post('email/send', 'EmailController@send');
