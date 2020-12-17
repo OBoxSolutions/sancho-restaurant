b57af749d58617333915ea45baf179054bbd6832
Jose Garcia <jogarcia@tuta.io>
2020-12-17 14:53:08 -0500

    Create basic menu structure

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -3,5 +3,83 @@
 @section('route', Route::currentRouteName())
 
 @section('content')
-    <vue-back-to-top></vue-back-to-top>
+<div class="row">
+    <h1>Sopas</h1>
+    <hr>
+    <vue-menu-item title="Jamón Ibérico de Bellota 5 Jotas">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Queso Manchego">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Boquerones a la Vinagreta">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Pulpo a la Vinagreta">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Jamón Serrano">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+</div>
+<div class="row">
+    <h1>Ensaladas</h1>
+    <hr>
+    <vue-menu-item title="Jamón Ibérico de Bellota 5 Jotas">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Queso Manchego">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Boquerones a la Vinagreta">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Pulpo a la Vinagreta">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Jamón Serrano">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+</div>
+<div class="row">
+    <h1>Paella</h1>
+    <hr>
+    <vue-menu-item title="Jamón Ibérico de Bellota 5 Jotas">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Queso Manchego">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Boquerones a la Vinagreta">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Pulpo a la Vinagreta">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Jamón Serrano">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+</div>
+<div class="row">
+    <h1>
+        Pescados y Mariscos
+    </h1>
+    <hr>
+    <vue-menu-item title="Jamón Ibérico de Bellota 5 Jotas">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Queso Manchego">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Boquerones a la Vinagreta">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Pulpo a la Vinagreta">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+    <vue-menu-item title="Jamón Serrano">
+        Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+    </vue-menu-item>
+</div>
+<vue-back-to-top></vue-back-to-top>
 @endsection
