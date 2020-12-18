e2f63f1736ed0d7d17fb271d1ec9629e93c22f38
Jose Manuel Cassola <jcassola@uclv.cu>
2020-12-17 23:13:41 -0500

    Send emails using queue

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Email;
+use App\Jobs\SendEmail;
 use App\Mail\OfferEmail;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -26,10 +28,11 @@ class EmailController extends Controller
 
 //        Mail::to('jcassola96@gmail.com')->send(new OfferEmail($data));
 
-//        $mailTo = Email::all();
-//        foreach ($mailTo as $mt){
+        $mailTo = Email::all();
+        foreach ($mailTo as $address){
 //            Mail::to($mt->email)->queue(new OfferEmail($data));
-//        }
+            SendEmail::dispatch($address, $data)->delay(Carbon::now()->addSecond(1));
+        }
 
         return new OfferEmail($data);
     }
