41c060b1236c5258659f132d5401e979d8fff308
Jose Garcia <jogarcia@tuta.io>
2020-12-19 14:53:42 -0500

    Refactor menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -4,91 +4,108 @@
 
 @section('content')
 <vue-section class="bg-white">
-    <h2>Sopas</h2>
-    <hr>
     <div class="row">
-        <vue-menu-item title="Jamón Ibérico de Bellota 5 Jotas">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Queso Manchego">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Boquerones a la Vinagreta">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Pulpo a la Vinagreta">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Jamón Serrano">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <h2>Sopas</h2>
+            <hr>
+            <vue-menu-item title="Jamón Ibérico de Bellota 5 Jotas">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+
+            <vue-menu-item title="Queso Manchego">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+            <vue-menu-item title="Boquerones a la Vinagreta">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+            <vue-menu-item title="Pulpo a la Vinagreta">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+            <vue-menu-item title="Jamón Serrano">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+        </div>
+        <div class="col-md-3"></div>
     </div>
 </vue-section>
 <vue-section class="bg-white">
 
-    <h2>Ensaladas</h2>
-    <hr>
     <div class="row">
-        <vue-menu-item title="Jamón Ibérico de Bellota 5 Jotas">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Queso Manchego">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Boquerones a la Vinagreta">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Pulpo a la Vinagreta">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Jamón Serrano">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <h2>Ensaladas</h2>
+            <hr>
+            <vue-menu-item title="Jamón Ibérico de Bellota 5 Jotas">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+            <vue-menu-item title="Queso Manchego">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+            <vue-menu-item title="Boquerones a la Vinagreta">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+            <vue-menu-item title="Pulpo a la Vinagreta">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+            <vue-menu-item title="Jamón Serrano">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+        </div>
     </div>
-</vue-section>
-<vue-section class="bg-white">
-    <h2>Paella</h2>
-    <hr>
-    <div class="row">
-        <vue-menu-item title="Jamón Ibérico de Bellota 5 Jotas">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Queso Manchego">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Boquerones a la Vinagreta">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Pulpo a la Vinagreta">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Jamón Serrano">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-    </div>
+    <div class="col-md-3"></div>
 
 </vue-section>
 <vue-section class="bg-white">
-    <h2>
-        Pescados y Mariscos
-    </h2>
-    <hr>
     <div class="row">
-        <vue-menu-item title="Jamón Ibérico de Bellota 5 Jotas">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Queso Manchego">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Boquerones a la Vinagreta">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Pulpo a la Vinagreta">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
-        <vue-menu-item title="Jamón Serrano">
-            Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
-        </vue-menu-item>
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <h2>Paella</h2>
+            <hr>
+            <vue-menu-item title="Jamón Ibérico de Bellota 5 Jotas">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+            <vue-menu-item title="Queso Manchego">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+            <vue-menu-item title="Boquerones a la Vinagreta">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+            <vue-menu-item title="Pulpo a la Vinagreta">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+            <vue-menu-item title="Jamón Serrano">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+        </div>
+        <div class="col-md-3"></div>
+    </div>
+</vue-section>
+<vue-section class="bg-white">
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <h2>
+                Pescados y Mariscos
+            </h2>
+            <hr>
+            <vue-menu-item title="Jamón Ibérico de Bellota 5 Jotas">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+            <vue-menu-item title="Queso Manchego">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+            <vue-menu-item title="Boquerones a la Vinagreta">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+            <vue-menu-item title="Pulpo a la Vinagreta">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+            <vue-menu-item title="Jamón Serrano">
+                Jamón cocinado a la ibérica, un plato delicioso y aún mas acompañada por uno de nuestros vinos
+            </vue-menu-item>
+        </div>
+        <div class="col-md-3"></div>
     </div>
 </vue-section>
 <vue-back-to-top></vue-back-to-top>
