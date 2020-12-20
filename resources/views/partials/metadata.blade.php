8b377b79f038c4635a6d3a4a85deb51667cf49ad
Jose Manuel Cassola <jcassola@uclv.cu>
2020-12-20 14:59:51 -0500

    Fix metadata

--- resources/views/partials/metadata.blade.php
+++ resources/views/partials/metadata.blade.php
@@ -1,26 +1,20 @@
 {{-- Web Information Meta Tags --}}
 <meta property="og:title" content="Sancho Restaurant" />
 <meta property="og:site_name" content="Sancho Restaurant"/>
-<meta property="og:description" content="Bienvenido al Sancho Restaurant, donde nuestra especialidad es la deliciosa cocina
-                                        creada con el ingenio y la pasión,
-                                        inspirados en los fantásticos sabores de España.
-                                        ¡Venga y experimente la cocina española a nuestra manera!"/>
+<meta property="og:description" content="Bienvenido al Sancho Restaurant, donde nuestra especialidad es la deliciosa cocina creada con el ingenio y la pasión, inspirados en los fantásticos sabores de España. ¡Venga y experimente la cocina española a nuestra manera!"/>
 <meta property="og:keywords" content="restaurant, food, drinks, good time, family"/>
 <meta property="og:author" content="Sancho Restaurant"/>
 <meta property="og:type" content="website" />
 <meta property="og:url" content="https://sancho-restaurant.herokuapp.com/"/>
-<meta property="og:image" content="{{ asset('logo.png') }}"/>
+<meta property="og:image" content="{{ asset('chicken.jpeg') }}"/>
 <meta property="og:image:size" content="300" />
 <meta property="og:locale" content="es_ES" />
 <meta property="og:copyright" content="Sancho Copyright 2020" />
 
 {{-- HTML5 default meta fields --}}
-<meta name="description" content="Bienvenido al Sancho Restaurant, donde nuestra especialidad es la deliciosa cocina
-      creada con el ingenio y la pasión,
-      inspirados en los fantásticos sabores de España.
-      ¡Venga y experimente la cocina española a nuestra manera!"/>
+<meta name="description" content="Bienvenido al Sancho Restaurant, donde nuestra especialidad es la deliciosa cocina creada con el ingenio y la pasión, inspirados en los fantásticos sabores de España. ¡Venga y experimente la cocina española a nuestra manera!"/>
 <meta name="keywords" content="restaurant, food, drinks, good time, family">
-<meta name="author" content="OBSolutions">
+<meta name="author" content="Sancho Restaurant">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 {{-- Search Engine Optimization Meta Tags --}}
