3626a229a2ddc299f4d844cf49156a618ea20818
Jose Garcia <jogarcia@tuta.io>
2021-04-18 10:27:06 -0400

    Remove container div

--- resources/js/components/BootstrapNavbar.vue
+++ resources/js/components/BootstrapNavbar.vue
@@ -1,32 +1,30 @@
 <template>
   <nav class="navbar navbar-expand-sm navbar-dark bg-primary-color">
-    <div class="container">
-      <a class="navbar-brand" href="/">
-        <img src="/assets/logo.webp" alt="Logo" style="width: 240px" />
-      </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
+    <a class="navbar-brand" href="/">
+      <img src="/assets/logo.webp" alt="Logo" style="width: 240px" />
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- Right Side Of Navbar -->
-        <bootstrap-navbar-links
-          :user-name="userName"
-          :current-route="currentRoute"
-        >
-          <!-- Authentication Links -->
-          <slot></slot>
-        </bootstrap-navbar-links>
-        <!-- <vue-theme-switcher></vue-theme-switcher> -->
-      </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Right Side Of Navbar -->
+      <bootstrap-navbar-links
+        :user-name="userName"
+        :current-route="currentRoute"
+      >
+        <!-- Authentication Links -->
+        <slot></slot>
+      </bootstrap-navbar-links>
+      <!-- <vue-theme-switcher></vue-theme-switcher> -->
     </div>
   </nav>
 </template>
